9a4c1f44fdd9b6dcaa91c50b4da9b63dc1422627
Austinbabe <yo861015@gmail.com>
2021-04-03 18:08:44 +0800

    chore: update designerPdfViewer's comment

--- src/easy/designerPdfViewer.js
+++ src/easy/designerPdfViewer.js
@@ -3,6 +3,7 @@ function designerPdfViewer(h, word) {
 	let highest = 0;
 	for (let i = 0; i < word.length; i++) {
 		let index = word[i].charCodeAt(0) - 97;
+		//set ASCCII index to alphabet index; (ASCII start at 97 with lowercase)
 		if (h[index] > highest) {
 			highest = h[index];
 		}
