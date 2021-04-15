1f68083107bc48ed71c4b2863efc98662cd60a7f
Austin <yo861015@gmail.com>
2021-04-15 16:17:15 +0800

    chore: update appleAndOrange wording

--- src/easy/appleAndOrange.js
+++ src/easy/appleAndOrange.js
@@ -6,27 +6,27 @@ t => tail(end) position of house
 range => bewteen s and t
 a, b => tree position 
 apples, oranges => array that record shifting of each fruit
-GOAL: counting how many fruites in range after shifting.
-Joke: House and fruits are tape with mass magnet in this quesiton :p
+Goal: counting how many fruites in range after shifting.
+Joke: House and fruits are tape with mass of magnet in this quesiton :p
 */
 
 //apporaching 1 brute force
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-  let apple = 0;
-  apples.forEach((item) => {
-    let temp = a + item;
-    if (temp >= s && temp <= t) {
-      apple++;
-    }
-  });
+	let apple = 0;
+	apples.forEach((item) => {
+		let temp = a + item;
+		if (temp >= s && temp <= t) {
+			apple++;
+		}
+	});
 
-  let orange = 0;
-  oranges.forEach((item) => {
-    let temp = b + item;
-    if (temp >= s && temp <= t) {
-      orange++;
-    }
-  });
-  console.log(apple);
-  console.log(orange);
+	let orange = 0;
+	oranges.forEach((item) => {
+		let temp = b + item;
+		if (temp >= s && temp <= t) {
+			orange++;
+		}
+	});
+	console.log(apple);
+	console.log(orange);
 }
