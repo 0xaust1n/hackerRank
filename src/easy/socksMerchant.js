9aaf4d626cc28a73b7555629b5e40f0a7f92ce06
Austin <yo861015@gmail.com>
2021-05-10 11:07:21 +0800

    chore: add clean solution in socksMerchant

--- src/easy/socksMerchant.js
+++ src/easy/socksMerchant.js
@@ -1,18 +1,29 @@
 // Complete the sockMerchant function below.
 function sockMerchant(n, ar) {
-  let dict = new Map();
-  let count = 0;
-  for (let i = 0; i < ar.length; i++ ) {
-    if (dict.has(ar[i])) {
-      let tempCount = parseInt(dict.get(ar[i])) + 1;
-      dict.set(ar[i], tempCount)
-      if (tempCount % 2 == 0) {
-        count++
-      }
-    }
-    else{
-      dict.set(ar[i], 1);
-    }
-  }
-  return count++
+	let dict = new Map();
+	let count = 0;
+	for (let i = 0; i < ar.length; i++) {
+		if (dict.has(ar[i])) {
+			let tempCount = parseInt(dict.get(ar[i])) + 1;
+			dict.set(ar[i], tempCount);
+			if (tempCount % 2 == 0) {
+				count++;
+			}
+		} else {
+			dict.set(ar[i], 1);
+		}
+	}
+	return count++;
 }
+
+// Clean Way
+const socksMerchant = (n, ary) => {
+	let map = new Map();
+	let c = 0;
+	ar.forEach((x) => {
+		let temp = map.has(x) ? map.get(x) + 1 : 1;
+		c = temp % 2 == 0 ? c + 1 : c;
+		map.set(x, temp);
+	});
+	return c;
+};
